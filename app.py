cc83ee71a381481605114f3cdb3af3288d64553b
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-06 04:08:16 +0530

    Update 06 Aug 2025

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ import random
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+        return render_template('index.html')
 
 
+if __name__ == "__main__":
+        socketio.run(app)
