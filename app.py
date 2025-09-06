eb2b3e68d9be55f2bc31cb10c1f117a08bc25bce
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-09-06 06:09:41 +0530

    Users and avatar modifi

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import random
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# dictionary. Store connected user. Key is socketid value is username and avatarurl.
+users = {}
+
+
 @app.route('/')
 def index():
         return render_template('index.html')
@@ -14,8 +18,10 @@ def index():
 def handle_connect():
         username = f"User_{random.randint(1000,9999)}"
         gender = random.choice(["girl","boy"])
-        # https://avatar.iran.liara.run/public/boy
-        #https://avatar.iran.liara.run/public/girl
+        # https://avatar.iran.liara.run/public/boy?username=Scott
+        # https://avatar.iran.liara.run/public/girl?username=Maria
+
+        avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
 
 if __name__ == "__main__":
         socketio.run(app)
